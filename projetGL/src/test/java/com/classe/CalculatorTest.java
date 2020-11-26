385421986d9c4e1eee49a3853d68eb0dd6a47a0d
01Ndeye <nsene63@gmail.com>
2020-11-26 18:26:32 +0000

    ajout du DockerFile

--- projetGL/src/test/java/com/classe/CalculatorTest.java
+++ projetGL/src/test/java/com/classe/CalculatorTest.java
@@ -18,10 +18,12 @@ public class CalculatorTest extends TestCase  {
 	public void testSum() {
 		Assert.assertEquals(4, cal.sum(2, 2));
 	}
+	
 	@Test
 	public void testMinus() {
 		Assert.assertEquals(0, cal.minus(2, 2));
 	}
+	
 	@Test
 	public void testDivide() {
 		Assert.assertEquals(2, cal.divide(6, 3));
@@ -44,5 +46,18 @@ public class CalculatorTest extends TestCase  {
 		Assert.assertEquals(2000,max);
 	}
 	
+	@Test
+	public void testMinElement(){
+		int tab[]= {2,1,8};
+		int min = cal.minElement(tab);
+		if (min != 1) Assert.fail();
+	}
+
+	@Test 
+	public void testMaxElement(){
+		int tab[]= {2,1,8};
+		int min = cal.maxElement(tab);
+		if (min != 8) Assert.fail();
+	}
 
 }
